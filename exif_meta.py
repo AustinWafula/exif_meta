08d83328a02158d8b811119a6589c9a1224029bb
Austin Wafula <54513416+AustinWafula@users.noreply.github.com>
2020-03-30 09:23:40 -0400

    Create exif_meta.py

---
+++ exif_meta.py
@@ -0,0 +1,31 @@
+from PIL import Image
+from PIL.ExifTags import TAGS
+import os
+
+path=input("Drive/Directory path:")
+k=('tiff','jpg','png','gif','jpe','jpeg','jif','jfi','jfif','webp','tif','raw','arw','cr2','nrw','k25')#image file extensions
+def get_meta():
+    for root, dirs, files in os.walk(path):
+        
+        for file in files:
+            for r in k:
+                if file.endswith(r):
+                    try:
+                        
+                        img=Image.open(file)
+                        width,height=img.size
+                        print('')
+                        print(file)
+                        print('Type:',img.format,'\n'+'Dimension:',img.size,'\n','Mode:',img.mode+'\n',str(os.path.abspath(file)))
+                        meta=img._getexif()
+                        for tag,value in meta.items():
+  
+                            d=TAGS.get(tag)
+                               
+                            exif=value
+                            print('============================')
+                            print(str(d),str(exif))
+                            print('')
+                    except (FileNotFoundError,AttributeError):
+                        pass
+get_meta()
